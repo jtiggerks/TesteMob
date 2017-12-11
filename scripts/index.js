12f61383018fdaae55b19193d39987cfe317207d
jtiggerks <jtiggerks@gmail.com>
2017-12-11 13:41:56 -0200

    1

--- scripts/index.js
+++ scripts/index.js
@@ -62,12 +62,13 @@ var app = {
 
 
           var urlfull= 'http://www.player100regras.com.br?p=1&u=';        
-          window.open(urlfull+device.uuid, '_self');   
+          window.open(urlfull+device.uuid, '_self');  
+           
           var notificationOpenedCallback = function(jsonData) {
             //console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
           };
 
-                           
+
 
           },
     // Update DOM on a Received Event
