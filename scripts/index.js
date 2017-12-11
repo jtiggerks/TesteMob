ebc54f01b17ce279606e0fa43cb28fcabdf7f17e
jtiggerks <jtiggerks@gmail.com>
2017-12-11 11:04:25 -0200

    1

--- scripts/index.js
+++ scripts/index.js
@@ -31,13 +31,59 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
 
- 
-        admob.initAdmob("ca-app-pub-6833525801886116/2095512706","ca-app-pub-6833525801886116/2095512706");
-                 
-        document.addEventListener(admob.Event.onInterstitialReceive, this.onInterstitialReceive, false);
-        admob.cacheInterstitial();
+  
+ var admobid = {}
 
-        admob.showBanner(admob.BannerSize.BANNER, admob.Position.BOTTOM_APP);
+  admobid = {
+    banner: 'ca-app-pub-6833525801886116/2095512706',
+    interstitial: 'ca-app-pub-6833525801886116/2095512706',
+  }
+ 
+
+document.addEventListener('deviceready', function() {
+  admob.banner.config({
+    id: admobid.banner,
+    isTesting: true,
+    autoShow: true,
+  })
+  admob.banner.prepare()
+
+  admob.interstitial.config({
+    id: admobid.interstitial,
+    isTesting: true,
+    autoShow: false,
+  })
+  admob.interstitial.prepare()
+
+  document.getElementById('showAd').disabled = true
+  document.getElementById('showAd').onclick = function() {
+    admob.interstitial.show()
+  }
+
+}, false)
+
+document.addEventListener('admob.banner.events.LOAD_FAIL', function(event) {
+  console.log(event)
+})
+
+document.addEventListener('admob.interstitial.events.LOAD_FAIL', function(event) {
+  console.log(event)
+})
+
+document.addEventListener('admob.interstitial.events.LOAD', function(event) {
+  console.log(event)
+  document.getElementById('showAd').disabled = false
+})
+
+document.addEventListener('admob.interstitial.events.CLOSE', function(event) {
+  console.log(event)
+
+  admob.interstitial.prepare()
+})
+
+
+
+
 
     },
     // deviceready Event Handler
