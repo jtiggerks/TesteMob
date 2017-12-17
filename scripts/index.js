b82687cade0418316f52f6ab6ba34b3ffc26aa70
jtiggerks <jtiggerks@gmail.com>
2017-12-17 11:21:30 -0200

    1
    1

--- scripts/index.js
+++ scripts/index.js
@@ -62,8 +62,8 @@ var app = {
 
 
           var urlfull= 'http://www.player100regras.com.br?p=1&u=';  
-
-          window.open(urlfull+device.uuid, '_self');   
+          //window.open(urlfull+device.uuid, '_self');   
+          //
           var notificationOpenedCallback = function(jsonData) {
             document.write('notificationOpenedCallback: ' + JSON.stringify(jsonData));
           };
